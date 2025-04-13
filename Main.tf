7a7b1e8dd41096485f202391cd31106596abb89e
Krishnamoorthy06 <krishnamoorthybe@yahoo.com>
2025-04-13 08:23:16 +0530

    Dev
    DevResources Updated

---
+++ Main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.1.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+
+  subscription_id = "26b831bd-b071-4f98-9962-57497a5e31a7"
+}
